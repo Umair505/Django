6ec4d5652704bf2435148fe09f808bb4e48285e5
Moinul Islam Umair <153224009+Umair505@users.noreply.github.com>
2025-02-10 10:01:49 +0600

    Add files via upload

---
+++ Django Module/23 Module Mamar Bank part-3/myproject/transactions/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Transaction,Transfer
+from .views import send_transaction_email
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['account', 'amount', 'balance_after_transaction',
+                    'transaction_type', 'loan_approve', 'get_is_bankrupt']
+
+    def save_model(self, request, obj, form, change):
+        obj.account.balance += obj.amount
+        obj.balance_after_transaction = obj.account.balance
+        obj.account.save()
+        send_transaction_email(obj.account.user, obj.amount,'Loan Approval','transactions/admin_email.html')
+        super().save_model(request, obj, form, change)
+
+    def get_is_bankrupt(self, obj):
+        if obj.account and hasattr(obj.account, 'is_bankrupt'):
+            return obj.account.is_bankrupt.is_bankrupt
+        else:
+            return False
+
+    get_is_bankrupt.short_description = 'Is Bankrupt'
+    get_is_bankrupt.boolean = True
+admin.site.register(Transfer)
