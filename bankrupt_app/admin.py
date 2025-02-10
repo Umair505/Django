fe879542e427a6f9319e32ad2f7dba79b403113c
Moinul Islam Umair <153224009+Umair505@users.noreply.github.com>
2025-02-10 10:06:05 +0600

    Delete bankrupt_app directory

--- bankrupt_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import IsBankrupt
-
-class IsBankruptAdmin(admin.ModelAdmin):
-    list_display = ('is_bankrupt',)  
-
-admin.site.register(IsBankrupt, IsBankruptAdmin)
--- bankrupt_app/views.py
+++
@@ -1,8 +0,0 @@
-
-
-from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
-
-class AmniView(TemplateView):
-    template_name = 'amni_test.html'
