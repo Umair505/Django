6b16a52abae3dd572c5245aa2d6d370aabbdfa43
Umair505 <moinuli359@gmail.com>
2025-02-17 09:49:12 +0600

    conceptual session project

---
+++ rest_blog/accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('author', 'Author'),
+        ('editor', 'Editor'),
+        ('admin', 'Admin')
+    )
+    user_type = models.CharField(max_length=10,choices=USER_TYPE_CHOICES,default='viewer')
