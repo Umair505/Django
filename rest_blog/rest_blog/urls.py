5cb9c92aae69cfa64dcf3685110cf950b5d14371
Umair505 <moinuli359@gmail.com>
2025-02-18 08:49:06 +0600

    Module 26.5 added

--- rest_blog/rest_blog/urls.py
+++ rest_blog/rest_blog/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/posts/', include("posts.urls")),
-    path('api-auth/', include("rest_framework.urls")),
     
+    # API Routes
+    path('api/posts/', include("posts.urls")),
+    path('api-auth/', include("rest_framework.urls")),  # DRF's browsable API login/logout
+
+    # Authentication Routes
+    path('api/auth/', include("dj_rest_auth.urls")),  # Login, Logout, Password Reset
+    path('api/auth/registration/', include("dj_rest_auth.registration.urls")),  # Registration
 ]
